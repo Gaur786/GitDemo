eb86c01e78999ffdc69b248417f9c431e1649b60
Abhishek Gaur <abhishekgaur257@gmail.com>
2023-01-18 15:44:22 +0530

    update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 print("Hello iNeuron !!!")
 print("hello")
 print("Hello iNeuron !!!")
-print("hello")print("Hello iNeuron !!!")
+print("hello")
 print("hello")print("Hello iNeuron !!!")
 print("hello")print("Hello iNeuron !!!")
 print("hello")print("Hello iNeuron !!!")
